3668f05433efd356d884dec26b3a6840e2b6454c
Robson Novato <rnlobao42@gmail.com>
2021-05-25 23:11:01 -0300

    Começando com estrutura

---
+++ prototipo0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    double M[12][12];//utilize esta matriz para ler os 144 numeros reais
+    char O;
+    scanf('%c', &O)
+    print(O)
+
+    return 0;//não remova
+}
