c407376999a643d6114189cc8aeb75b9865b16f3
Robson Novato <rnlobao42@gmail.com>
2021-05-31 20:33:49 -0300

    pequena adaptação

--- aulas/aula1.c
+++ aulas/aula1.c
@@ -6,17 +6,10 @@
 
 //Usando o *p voce pega o conteúdo de p, que no caso é o valor que voce atribuiu. 
 
+
 #include <stdio.h>
 
 int main() {
-    int k;
-    int *p;
-    p = &k; //p recebe o endereço de a (o p está apontando pra a, sempre que mudar o p o a muda)
-    *p = 8; //conteúdo de p alterado pra 8 e consequentemente alterando o conteúdo de a
-    printf("O valor de a: %d", k);
-
-
-
     int a = 5, b = 7;
     troca(&a, &b); //tem que passar o endereço de a e b
     printf("\n%d %d", a, b);
@@ -24,10 +17,11 @@ int main() {
     return 0;
 }
 
-//exercício que troca:
 void troca (int *px, int *py) {
     int temp;
     temp = *px;
     *px = *py;
     *py = temp;
 }
+
+
