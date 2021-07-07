b249dc14a3d6acfeb858135f616179fbdb4007f0
Robson Novato <rnlobao42@gmail.com>
2021-07-07 19:05:21 -0300

    prática de segunda

--- Estrutura-de-dados-1/Prática/ex05P/lista.c
+++ Estrutura-de-dados-1/Prática/ex05P/lista.c
@@ -7,7 +7,7 @@
 void TLista_Inicia(TLista *pLista) {
     pLista->pPrimeiro = (TCelula*) malloc (sizeof(TCelula));
     pLista->pUltimo = pLista->pPrimeiro;
-    pLista->pPrimeiro->pProx = NULL;
+    pLista->pPrimeiro->pProx = NULL; //isso é iniciando com cabeça
 }
 
 //Retorna se a lista esta vazia
@@ -44,19 +44,33 @@ void TLista_Imprime(TLista *pLista) {
         printf("%s ", pAux->item.nome);
         pAux = pAux->pProx;
     }
+    //printf("%s\n", pAux->item.nome);
 }
 
 //Remove cada elemento de uma lista e libera a memória
 void TLista_Esvazia(TLista *pLista) {
-
+    TCelula *aux;
+    while (pLista->pPrimeiro->pProx != NULL) {
+        aux = pLista->pPrimeiro->pProx;
+        pLista->pPrimeiro->pProx = aux->pProx;
+        free(aux);
+    }
 }
 
 // Acrescenta o conteudo de uma lista ao final de outra, apenas manipulando ponteiros
 void TLista_append(TLista *pLista1, TLista *pLista2){
-
+    pLista1->pUltimo->pProx = pLista2->pPrimeiro->pProx;
+    pLista2->pUltimo = pLista2->pUltimo;
 }
 
 // Inclui o conteudo de uma lista em outra, na posicao anterior a str, apenas manipulando ponteiros
 void TLista_include(TLista *pLista1, TLista *pLista2, char *str){
-
+    TCelula *aux, *aux2;
+    aux = pLista1->pPrimeiro;
+    while(strcmp(aux->item.nome, str) != 0) {
+        aux2 = aux;
+        aux = aux->pProx;  
+    }
+    aux2->pProx = pLista2->pPrimeiro->pProx;
+    pLista2->pUltimo->pProx = aux;
 }
--- Estrutura-de-dados-1/Prática/ex05P/principal.c
+++ Estrutura-de-dados-1/Prática/ex05P/principal.c
@@ -11,6 +11,8 @@ int main()
     TItem x;
 
     //iniciar listas
+    TLista_Inicia(&original);
+    TLista_Inicia(&nova);
 
     //le a primeira linha
     fgets (str, MAX_STR, stdin);
@@ -23,18 +25,45 @@ int main()
     while (token != NULL) {
       strcpy(x.nome, token);
       //inserir o item na lista adequada
+      TLista_InsereFinal(&original, x);
       token = strtok(NULL, " ");
     }
 
+
+
+
     //ler a segunda linha, igual a primeira e preencher a segunda lista
+    fgets (str2, MAX_STR, stdin);
+    //remove o \n incluido pelo fgets
+    str2[strcspn(str2, "\n")] = 0;
+
+    //tokenizacao da string original, divide em strings delimitadas por espaco em branco
+    token = strtok(str2, " ");
+
+    while (token != NULL) {
+      strcpy(x.nome, token);
+      //inserir o item na lista adequada
+      TLista_InsereFinal(&nova, x);
+      token = strtok(NULL, " ");
+    }
+
 
     //ler a terceira linha, usando apenas scanf
+    scanf(" %s", str3);
+
 
     //dependendo se ha indicacao ou nao, manipular as listas de acordo
 
+    if (strcmp(str3, "nao") == 0) {
+      TLista_append(&original, &nova);
+    }
+
+    else {
+      TLista_include(&original, &nova, str3);
+    }
     //imprimir a lista final
-
+    TLista_Imprime(&original);
     //ao final as duas listas estarao unificadas, entao basta esvaziar a original
-
+    TLista_Esvazia(&original);
     return 0;//nao remova
 }
