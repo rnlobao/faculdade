bfbf472338edc352065f9121df18c03e6d6b97ee
Robson Novato <rnlobao42@gmail.com>
2022-09-05 21:57:00 -0300

    removendo itens desnecessarios

--- 2° Período/BCC202/Prática/tp3P/caio/buscabinaria.h
+++
@@ -1,7 +0,0 @@
-# ifndef buscabinaria_h
-# define buscabinaria_h
-
-double buscaBinaria(double area, double tira[], int n, double begin, double end);
-void imprimirRESULT(double areaTira, double a, double tira[], int n, double maior);
-
-# endif
