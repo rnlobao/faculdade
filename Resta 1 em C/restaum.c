5443f7362faebacc010e6d956e3196b49e108026
Robson Novato <rnlobao42@gmail.com>
2022-09-06 09:48:27 -0300

    reescrendo resta um

--- Resta 1 em C/restaum.c
+++ Resta 1 em C/restaum.c
@@ -1,3 +1,13 @@
-//Nome: Robson Novato Lobao
-//Matricula: 20.1.4018
+//Nome: Robson Novato Lobão
+//Matricula:20.1.4018
 //Turma: BCC201 Turma 41
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+// fores e formato de texto
+#define ANSI_RESET "\x1b[0m"
+#define ANSI_BOLD "\x1b[1m"
+
