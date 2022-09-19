860502cd3a703348d9577aa17f8e40690245bde0
Robson Novato <rnlobao42@gmail.com>
2022-09-19 17:26:25 -0300

    creating files

---
+++ carteirada-front-end/Pods/Target Support Files/Pods-carteirada-front-end-carteirada-front-endUITests/Pods-carteirada-front-end-carteirada-front-endUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_carteirada_front_end_carteirada_front_endUITests : NSObject
+@end
+@implementation PodsDummy_Pods_carteirada_front_end_carteirada_front_endUITests
+@end
