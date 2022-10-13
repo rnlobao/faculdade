6733ab3b823596d0bdf9e856ae9ffa0361496948
Robson Novato <rnlobao42@gmail.com>
2022-10-12 23:16:49 -0300

    chart almost done

--- carteirada-front-end/carteirada-front-end/Home/Cells/ChartTableViewCell.swift
+++ carteirada-front-end/carteirada-front-end/Home/Cells/ChartTableViewCell.swift
@@ -19,9 +19,7 @@ class ChartTableViewCell: UITableViewCell, ChartViewDelegate {
     @IBOutlet weak var percentButton: UIButton!
     @IBOutlet weak var posicaoAtualLabel: UILabel!
     @IBOutlet weak var rendimentoLabel: UILabel!
-    
-    var hisTotalMoney: Double = 0.0
-        
+            
     @IBAction func showPercent(_ sender: Any) {
         var keyToPercent: Bool
         keyToPercent = pieChart.isUsePercentValuesEnabled == true ? true : false
@@ -39,51 +37,67 @@ class ChartTableViewCell: UITableViewCell, ChartViewDelegate {
         percentButton.layer.cornerRadius = 5
         pieChart.delegate = self
         setupHisTotalMoney()
-        setupChart()
     }
     
     func setupHisTotalMoney() {
+        var totalMoney: Double = 0.0
+        var totalAcoesMoney: Double = 0.0
+        var totalFiiMoney: Double = 0.0
+        var totalCriptoMoney: Double = 0.0
+        var entries = [ChartDataEntry]()
+
         let ref = Database.database().reference()
         let uid = Auth.auth().currentUser?.uid
         
         ref.child("users").child(uid!).child("assets").child("totalCarteira").observe(.value) { data in
-            self.hisTotalMoney = data.value as! Double
+            totalMoney = data.value as! Double
+            self.setupCentralText(total: totalMoney)
         }
         
+        ref.child("users").child(uid!).child("assets").child("acoes").child("inTotal").observe(.value) { data in
+            totalAcoesMoney = data.value as! Double
+            entries.append(ChartDataEntry(x: totalAcoesMoney, y: totalAcoesMoney))
+            self.setupChart(entries: entries)
+        }
+        
+        ref.child("users").child(uid!).child("assets").child("fii").child("inTotal").observe(.value) { data in
+            totalFiiMoney = data.value as! Double
+            entries.append(ChartDataEntry(x: totalFiiMoney, y: totalFiiMoney))
+            self.setupChart(entries: entries)
+        }
+        
+        ref.child("users").child(uid!).child("assets").child("cripto").child("inTotal").observe(.value) { data in
+            totalCriptoMoney = data.value as! Double
+            entries.append(ChartDataEntry(x: totalCriptoMoney, y: totalCriptoMoney))
+            self.setupChart(entries: entries)
+        }
     }
     
-    func setupChart() {
+    func setupChart(entries: [ChartDataEntry]) {
         pieChart.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         pieChart.center = self.center
         self.addSubview(pieChart)
         
-        var entries = [ChartDataEntry]()
-        
-        for x in 0..<10 {
-            entries.append(ChartDataEntry(x: Double(x), y: Double(x)))
-        }
-        
         let set = PieChartDataSet(entries: entries)
-        set.colors = ChartColorTemplates.colorful()
+        set.valueFont = UIFont(name: "Poppins-SemiBold", size: 12)!
+        set.colors = ChartColorTemplates.pastel()
         let data = PieChartData(dataSet: set)
         pieChart.data = data
         
-        let myMoney = String(hisTotalMoney)
         
-        let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.black,
-                           NSAttributedString.Key.font: UIFont(name: "Poppins-ExtraBold", size: 15)]
-        let myAttrString = NSAttributedString(string: myMoney, attributes: attributes as [NSAttributedString.Key : Any])
-        pieChart.centerAttributedText = myAttrString
-        pieChart.drawCenterTextEnabled = true
         pieChart.legend.enabled = false
         pieChart.rotationEnabled = false
         
         pieChart.transparentCircleColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.0)
-        
-        
     }
     
     func setupCentralText(total: Double) {
+        let myMoney = String(total)
         
+        let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.black,
+                           NSAttributedString.Key.font: UIFont(name: "Poppins-ExtraBold", size: 15)]
+        let myAttrString = NSAttributedString(string: "R$ \(myMoney)", attributes: attributes as [NSAttributedString.Key : Any])
+        pieChart.centerAttributedText = myAttrString
+        pieChart.drawCenterTextEnabled = true
     }
 }
